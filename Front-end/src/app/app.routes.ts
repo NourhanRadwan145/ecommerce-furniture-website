919914b962c93bd6d06e36f725e5aa2cc90eea15
Zeinab Abdelghaffar <zeinanabdo88@gmail.com>
2024-04-17 00:48:14 +0200

    add the project components and routes

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ItemComponent } from './components/item/item.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'item', component: ItemComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'products', component: ProductsComponent }, 
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', redirectTo: '/home' } 
+
+];
