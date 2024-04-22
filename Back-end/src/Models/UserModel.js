6c6f6805baa621ed74d7c1ca693cbfa9fd49459f
unknown <oudaa898@gmail.com>
2024-04-22 02:01:34 +0200

    update node-module file

--- Back-end/src/Models/UserModel.js
+++ Back-end/src/Models/UserModel.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-    "username": String,
-    "password": String,
-    "email": String,
-    "gender": String,
+    "username": {type:String,
+        required:true,
+    },
+    "password": {type:String,
+        required:true,
+    },
+    "email": {type:String,
+        unique:true,
+        required:true,
+    },
+    "gender": { 
+        type: String, 
+        enum: ['male', 'female']
+    },
     "image": String, 
     "orders": [ { type: mongoose.Schema.Types.ObjectId, ref: "orders" }, ],
     "carts": [ { type: mongoose.Schema.Types.ObjectId, ref: "products" }, ],
-    "isAdmin": Boolean
+    "isAdmin": { type: Boolean, default: false } 
 })
 
 module.exports = mongoose.model("users", UserSchema)
--- Back-end/src/Servers/server.js
+++ Back-end/src/Servers/server.js
@@ -9,11 +9,18 @@ const productRoute = require('../Routes/productsRoute');
 const orderRoute = require('../Routes/OrderRoute');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 //#endregion
 
 mongoose.connect("mongodb://127.0.0.1:27017/E-Commerce");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(cors({
+    credentials:true,
+    origin:["*"]
+}))
+app.use(cookieParser())
 
 //#region Routes
 app.use("/api/users", UserRoutes)
