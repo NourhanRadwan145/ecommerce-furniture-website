94f622fb9c145b71d00bda6925a1e7c862a590da
nadarabea1 <nadarabea81@gmail.com>
2024-04-28 21:32:29 +0300

    feat: update the product's qunatity

--- Back-end/src/Controllers/UserController.js
+++ Back-end/src/Controllers/UserController.js
@@ -1,5 +1,6 @@
 // ---------------------------------- All Requires -------------------------------------
 const UserModel = require("../Models/UserModel")
+const ProductModel = require("../Models/productsModel")
 const UserValidate = require("../Utils/UserValidate")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
@@ -35,6 +36,11 @@ const AddProductToCart = async (req, res) => {
     const { userid, productid, quantity } = req.body;
 
     try {
+        const product = await ProductModel.findById(productid);
+        if (!product || product.quantity <= 0) {
+            return res.status(404).json({ message: "Product not found or out of stock" });
+        }
+
         const updatedUser = await UserModel.findOneAndUpdate(
             { _id: userid },
             { $push: { carts: { product: productid, quantity } } },
@@ -53,28 +59,47 @@ const AddProductToCart = async (req, res) => {
 
 
 
+
 // ---------------------------------- Remove Product From Cart ------------------------
 let RemoveProductFromCart = async (req, res) => {
     const { userid, productid } = req.body;
 
     try {
-        const updatedUser = await UserModel.findOneAndUpdate(
-            { _id: userid },
-            { $pull: { carts: { product: productid } } },
-            { new: true }
-        );
-
-        if (!updatedUser) {
-            return res.status(404).json({ message: "User not found" + userid });
+        const user = await UserModel.findById(userid);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
         }
+
+        const cartItem = user.carts.find(item => item.product.toString() === productid);
+        if (!cartItem) {
+            return res.status(404).json({ message: "Product not found in user's cart" });
+        }
+
+        const CartItemQunatity = cartItem.quantity;
+
+        user.carts = user.carts.filter(item => item.product.toString() !== productid);
+        await user.save();
+
+        const product = await ProductModel.findById(productid);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        product.quantity += CartItemQunatity;
+        await product.save();
+
         res.status(200).json({ message: "Product removed from cart successfully" });
 
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Server error" });
     }
+};
 
-}
+
+
+
+
 // ---------------------------------- Update Product Quantity ------------------------
 let IncreaseProductQuantity = async (req, res) => {
     const { userid, productid } = req.body;
@@ -89,14 +114,23 @@ let IncreaseProductQuantity = async (req, res) => {
         if (!updatedUser) {
             return res.status(404).json({ message: "User not found" });
         }
+
+        const product = await ProductModel.findById(productid);
+        if (!product || product.quantity == 0) {
+            return res.status(404).json({ message: "Product not found or out of stoke" });
+        }
+        product.quantity -= 1;
+        await product.save();
+
         res.status(200).json({ message: "Product quantity increased successfully" });
 
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Server error" });
     }
+};
 
-}
+
 let DecreaseProductQuantity = async (req, res) => {
     const { userid, productid } = req.body;
 
@@ -110,6 +144,14 @@ let DecreaseProductQuantity = async (req, res) => {
         if (!updatedUser) {
             return res.status(404).json({ message: "User not found" });
         }
+
+        const product = await ProductModel.findById(productid);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        product.quantity += 1;
+        await product.save();
+
         res.status(200).json({ message: "Product quantity decreased successfully" });
 
     } catch (error) {
@@ -117,12 +159,13 @@ let DecreaseProductQuantity = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 }
+
 // ---------------------------------- End Of Cart Operations ------------------------
 
 
-let UpdateUser = async (req, res)=>{
-    const id= req.params.id;  
-    const updateData = req.body;  
+let UpdateUser = async (req, res) => {
+    const id = req.params.id;
+    const updateData = req.body;
 
     try {
         // Find the user by ID and update
@@ -140,69 +183,69 @@ let UpdateUser = async (req, res)=>{
 
 
 // ---------------------------------- Delete User By ID  ---------------------------------
-let DeleteUser = async (req, res)=>{}
+let DeleteUser = async (req, res) => { }
 // ---------------------------------- Login User  ---------------------------------------
-let LoginUser = async (req, res)=>{
-    const user = await UserModel.findOne({email:req.body.email})
-    if(!user) {
-        return res.status(400).send({message:"Invalid Email or Password"})
+let LoginUser = async (req, res) => {
+    const user = await UserModel.findOne({ email: req.body.email })
+    if (!user) {
+        return res.status(400).send({ message: "Invalid Email or Password" })
     }
     const validPassword = await bcrypt.compare(req.body.password, user.password)
-    if(!validPassword) {
-        return res.status(400).send({message:"Invalid Email or Password"})
+    if (!validPassword) {
+        return res.status(400).send({ message: "Invalid Email or Password" })
     }
-    const {_id} = user.toJSON()
-    const token = jwt.sign({_id: _id }, "secret")
+    const { _id } = user.toJSON()
+    const token = jwt.sign({ _id: _id }, "secret")
     res.cookie("jwt", token, {
-        httpOnly:true,
-        maxAge: 24*30*60*60*1000
+        httpOnly: true,
+        maxAge: 24 * 30 * 60 * 60 * 1000
     })
-    return res.status(200).json({message:"User Logged In Successfully", user:user})
-    
+    return res.status(200).json({ message: "User Logged In Successfully", user: user })
+
 }
 // ---------------------------------- Register User  ------------------------------------
-let RegisterUser = async (req, res)=>{
-    let {error} = UserValidate(req.body)
-    if(error) return res.status(400).json({message:error.details[0].message})
+let RegisterUser = async (req, res) => {
+    let { error } = UserValidate(req.body)
+    if (error) return res.status(400).json({ message: error.details[0].message })
 
     try {
-        let name = req.body.username  
+        let name = req.body.username
         let email = req.body.email
         let password = req.body.password
         let gender = req.body.gender
 
-        let salt = await bcrypt.genSalt(10)  
-        let hashedPassword = await bcrypt.hash(password, salt) 
+        let salt = await bcrypt.genSalt(10)
+        let hashedPassword = await bcrypt.hash(password, salt)
 
-        let checkUser = await UserModel.findOne({email:email})    
-        if(checkUser){
-            return res.status(400).json({message:"User Already Exists"})
+        let checkUser = await UserModel.findOne({ email: email })
+        if (checkUser) {
+            return res.status(400).json({ message: "User Already Exists" })
         }
-        else{
-            let newUser = new UserModel({  
-                username:name,
-                email:email,
-                password:hashedPassword,
+        else {
+            let newUser = new UserModel({
+                username: name,
+                email: email,
+                password: hashedPassword,
                 gender: gender
             })
-            let savedUser = await newUser.save()  
-            const {_id} = savedUser.toJSON() 
+            let savedUser = await newUser.save()
+            const { _id } = savedUser.toJSON()
             // const secretKey = crypto.randomBytes(32).toString("hex") 
-            const token = jwt.sign({_id: _id}, "secret") 
-            res.cookie("jwt", token, { 
-                httpOnly:true,
-                maxAge: 24*30*60*60*1000 
-            })  
+            const token = jwt.sign({ _id: _id }, "secret")
+            res.cookie("jwt", token, {
+                httpOnly: true,
+                maxAge: 24 * 30 * 60 * 60 * 1000
+            })
             // console.log(token)
-            return res.status(201).json({message:"User Created Successfully", user:savedUser})  
+            return res.status(201).json({ message: "User Created Successfully", user: savedUser })
         }
-    } 
+    }
     catch (error) {
-        return res.status(500).json({message:error.message})
+        return res.status(500).json({ message: error.message })
     }
 }
 
 
 // ---------------------------------- Export All Functions  ------------------------------
-module.exports = {GetAllUsers, GetUserById, AddNewUser, UpdateUser, DeleteUser, LoginUser, RegisterUser, AddProductToCart, RemoveProductFromCart, IncreaseProductQuantity, DecreaseProductQuantity}
+module.exports = { GetAllUsers, GetUserById, AddNewUser, UpdateUser, DeleteUser, LoginUser, RegisterUser, AddProductToCart, RemoveProductFromCart, IncreaseProductQuantity, DecreaseProductQuantity }
 // ---------------------------------- End Of Controller ----------------------------------
